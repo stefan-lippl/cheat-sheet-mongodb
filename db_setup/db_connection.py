462a8c998e13e886c3c12dd21dfd8183e8f1c4b5
s.lippl <s.lippl@yahoo.com>
2021-10-07 18:52:02 +0200

    removed db_id

--- db_setup/db_connection.py
+++ db_setup/db_connection.py
@@ -5,6 +5,4 @@ class Connect(object):
     @staticmethod
     def get_connection():
         # Change <PW> with the personal mdb password
-        return MongoClient("mongodb://stefan-lippl:<PW>@cluster0-shard-00-00.301hm.mongodb.net:27017,"
-                           "cluster0-shard-00-01.301hm.mongodb.net:27017,cluster0-shard-00-02.301hm.mongodb.net:27017/"
-                           "test?ssl=true&replicaSet=atlas-luyu3t-shard-0&authSource=admin&retryWrites=true&w=majority")
+        return MongoClient("<DB_KEY>")
